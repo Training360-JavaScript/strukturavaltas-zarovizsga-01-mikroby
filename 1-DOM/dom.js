b2a6c75beae08eadb9ceeeb65ddba5af5b34130b
mikroby <rtamas@hotmail.com>
2022-02-28 14:30:57 +0100

    1.2 solved

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,12 @@
 function manipulateDom() {
-    // 
+
+    const quoteAll = document.querySelectorAll('.quote');
+
+    quoteAll.forEach(element => {
+        element.style.borderColor = 'blue';
+        element.style.backgroundColor = 'lightblue';
+        element.style.fontStyle = 'italic';
+    });
 };
 
 export { manipulateDom };
