b6bac21868b0057c5521cc3be3c3008d32d79823
mikroby <rtamas@hotmail.com>
2022-02-28 16:42:56 +0100

    5.solved

--- firebase-5/src/environments/environment.prod.ts
+++ firebase-5/src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBrqgwwbQpWyMKh_gL_fiXzZcabMUCJHlU",
+
+  authDomain: "mikrobypages.firebaseapp.com",
+
+  projectId: "mikrobypages",
+
+  storageBucket: "mikrobypages.appspot.com",
+
+  messagingSenderId: "75476334152",
+
+  appId: "1:75476334152:web:8193789ba063281227ca7d"
+
+};
+
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig,
 };
--- firebase-5/src/environments/environment.ts
+++ firebase-5/src/environments/environment.ts
@@ -2,8 +2,27 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBrqgwwbQpWyMKh_gL_fiXzZcabMUCJHlU",
+
+  authDomain: "mikrobypages.firebaseapp.com",
+
+  projectId: "mikrobypages",
+
+  storageBucket: "mikrobypages.appspot.com",
+
+  messagingSenderId: "75476334152",
+
+  appId: "1:75476334152:web:8193789ba063281227ca7d"
+
+};
+
+
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig,
 };
 
 /*
